a550c81bb3c8395ed9aaa585f2485c1efa120d89
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2016-05-27 15:53:14 +0200

    Disable injection of extension in open tabs for browsers that don't support it. Extension should be injected in tabs that are open at the time of installation. Disabling this injection means that the browser must be restarted to use the extension.

--- background-script.js
+++ background-script.js
@@ -1,26 +1,29 @@
 'use strict';
 
 //manually inject content script code after installation or update
-chrome.runtime.onInstalled.addListener(function(details) {
-	if(details.reason === 'install') {
-		var script = chrome.runtime.getManifest().content_scripts[0].js[0];
-		chrome.windows.getAll({populate: true}, function(windows) {
-			windows.forEach(function(win) {
-				win.tabs.forEach(function(tab) {
-					//exclude internal chrome web pages
-					if(!tab.url.startsWith('chrome')) {
-						try {
-							chrome.tabs.executeScript(tab.id, {file: script});
+//TODO this is only supported by Chrome
+if(chrome.runtime.onInstalled) {
+	chrome.runtime.onInstalled.addListener(function(details) {
+		if(details.reason === 'install') {
+			var script = chrome.runtime.getManifest().content_scripts[0].js[0];
+			chrome.windows.getAll({populate: true}, function(windows) {
+				windows.forEach(function(win) {
+					win.tabs.forEach(function(tab) {
+						//exclude internal chrome web pages
+						if(!tab.url.startsWith('chrome')) {
+							try {
+								chrome.tabs.executeScript(tab.id, {file: script});
+							}
+							catch(exception) {
+								//this may fail for some kind of tab
+							}
 						}
-						catch(exception) {
-							//this may fail for some kind of tab
-						}
-					}
+					});
 				});
 			});
-		});
-	}
-});
+		}
+	});
+}
 
 chrome.runtime.onMessage.addListener(
 	function(message) {
